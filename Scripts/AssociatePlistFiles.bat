bf8024f4ed4e11f0713336b8c7a2ef6d255b6afd
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-04-01 16:22:26 -0500

    Try to work around nonsense PATH vars

--- Scripts/AssociatePlistFiles.bat
+++ Scripts/AssociatePlistFiles.bat
@@ -24,27 +24,28 @@ REM Get one directory up
 pushd %~dp0
 cd ..\
 set "path=%cd%"
+set "regpath=%ComSpec:cmd.exe=%reg.exe"
 popd
 echo Checking if ProperTree.bat exists in registry...
-reg query "HKCR\Applications\ProperTree.bat" 2> nul
+"!regpath!" query "HKCR\Applications\ProperTree.bat" > nul 2>&1
 if "%errorlevel%"=="0" (
     echo  - Already exists.  Removing...
     echo.
-    reg delete "HKCR\Applications\ProperTree.bat" /f 2> nul
-    reg delete "HKCR\.plist_auto_file" /f 2> nul
-    reg delete "HKCR\.plist" /f 2> nul
-    reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\ProperTree.bat_.plist" /f 2> nul
+    "!regpath!" delete "HKCR\Applications\ProperTree.bat" /f 2> nul
+    "!regpath!" delete "HKCR\.plist_auto_file" /f 2> nul
+    "!regpath!" delete "HKCR\.plist" /f 2> nul
+    "!regpath!" delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\ProperTree.bat_.plist" /f 2> nul
 )
 set arg=\"%path%\ProperTree.bat\" \"%%1\"
 echo.
 echo Adding registry values...
 echo.
-reg add "HKCR\Applications\ProperTree.bat\shell\Open" /t REG_SZ /d "Open with ProperTree" /f
-reg add "HKCR\Applications\ProperTree.bat\shell\Open\command" /t REG_SZ /d "%arg%" /f
-reg add "HKCR\.plist" /t REG_SZ /d ".plist_auto_file" /f
-reg add "HKCR\.plist_auto_file\shell\Open" /t REG_SZ /d "Open with ProperTree" /f
-reg add "HKCR\.plist_auto_file\shell\Open\command" /t REG_SZ /d "%arg%" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\ProperTree.bat_.plist" /t REG_DWORD /d 0 /f
+"!regpath!" add "HKCR\Applications\ProperTree.bat\shell\Open" /t REG_SZ /d "Open with ProperTree" /f
+"!regpath!" add "HKCR\Applications\ProperTree.bat\shell\Open\command" /t REG_SZ /d "%arg%" /f
+"!regpath!" add "HKCR\.plist" /t REG_SZ /d ".plist_auto_file" /f
+"!regpath!" add "HKCR\.plist_auto_file\shell\Open" /t REG_SZ /d "Open with ProperTree" /f
+"!regpath!" add "HKCR\.plist_auto_file\shell\Open\command" /t REG_SZ /d "%arg%" /f
+"!regpath!" add "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\ProperTree.bat_.plist" /t REG_DWORD /d 0 /f
 echo.
 echo Press [enter] to exit...
 pause > nul
