161f15578afe1f0d408903a0dad45949f8f5edb3
rusty-bits <rusty_bits@aol.com>
2023-02-03 10:26:07 -0500

    merge 02-02-2023 edits from corp

--- Scripts/config_tex_info.py
+++ Scripts/config_tex_info.py
@@ -1,74 +1,130 @@
 import os
 import sys
-if sys.version_info.major == 3:
-    import tkinter as tk, tkinter.font as tk_font
+import re
+import json
+if sys.version_info.major >= 3:
+    import tkinter as tk
+    import tkinter.font as tk_font
+    from tkinter import messagebox as mb
 else:
-    import Tkinter as tk, tkFont as tk_font
+    import Tkinter as tk
+    import tkFont as tk_font
+    import tkMessageBox as mb
 
 
-def display_info_window(config_tex, search_list, width, valid_only, show_urls, mx, my):
+def display_info_window(config_tex, search_list, width, valid_only, show_urls, mx, my, font=None, fg="white", bg="black"):
     # probably a simpler way to set up the formatted text
     # but found this format online and it works for now
     class FormattedText(tk.Text):
         def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            default_font = tk_font.nametofont(self.cget("font"))
+            # super(FormattedText,self).__init__(*args, **kwargs)
+            tk.Text.__init__(self, *args, **kwargs)
+            self.font = kwargs.get("font")
+            self.update_font()
 
-            bold_font = tk_font.Font(**default_font.configure())
-            italic_font = tk_font.Font(**default_font.configure())
-            mono_font = tk_font.Font(**default_font.configure())
-            normal_font = tk_font.Font(**default_font.configure())
-            underline_font = tk_font.Font(**default_font.configure())
-            url_font = tk_font.Font(**default_font.configure())
+        def update_font(self, font=None):
+            self.font = font or self.font
+            if not self.font:  # Fall back to defaults
+                self.font = tk_font.nametofont(self.cget("font"))
 
-            bold_font.configure(weight="bold")
-            italic_font.configure(slant="italic")
-            mono_font.configure(family="Courier New")
-            underline_font.configure(underline=1)
-            url_font.configure(family="courier New")
+            self.bold_font = tk_font.Font(**self.font.configure())
+            self.italic_font = tk_font.Font(**self.font.configure())
+            self.mono_font = tk_font.Font(**self.font.configure())
+            self.normal_font = tk_font.Font(**self.font.configure())
+            self.underline_font = tk_font.Font(**self.font.configure())
+            self.url_font = tk_font.Font(**self.font.configure())
 
-            self.tag_configure("bold", font=bold_font)
-            self.tag_configure("italic", font=italic_font)
-            self.tag_configure("underline", font=underline_font)
-            self.tag_configure("mono", font=mono_font)
-            self.tag_configure("normal", font=normal_font)
-            self.tag_configure("url", font=url_font, foreground="blue")
+            self.bold_font.configure(weight="bold")
+            self.italic_font.configure(slant="italic")
+            self.mono_font.configure(family="Courier New")
+            self.underline_font.configure(underline=1)
+            self.url_font.configure(family="courier New")
+
+            self.tag_configure("bold", font=self.bold_font)
+            self.tag_configure("italic", font=self.italic_font)
+            self.tag_configure("underline", font=self.underline_font)
+            self.tag_configure("mono", font=self.mono_font)
+            self.tag_configure("normal", font=self.normal_font)
+            self.tag_configure("url", font=self.url_font, foreground="blue")
             self.tag_configure(
-                "reverse", font=normal_font, background="white", foreground="black")
+                "reverse", font=self.normal_font, background="white", foreground="black")
+
+    # Another online discovery for auto-hiding scrollbars in a text widget
+    class AutoHideScrollbar(tk.Scrollbar):
+        def set(self, low, high):
+            if float(low) <= 0 and float(high) >= 1.0:
+                self.tk.call("grid", "remove", self)
+            else:
+                self.grid()
+            tk.Scrollbar.set(self, low, high)
 
     result = parse_configuration_tex(
         config_tex, search_list, width, valid_only, show_urls)
 
+    pad_pixels = 30
     info_window = tk.Toplevel()
-    info_window.title(" > ".join(search_list))  # set title to search path
-    info_win_height = len(result) + 1  # size info_window to the search result
-#    if info_win_height > 40:
-#        info_win_height = 40
-    text = FormattedText(info_window, width=width, height=info_win_height)
-    text.pack(fill="both", expand=True)
-    #move window to mx, my
-    info_window.geometry("+%d+%d" % (mx, my))
+    info_window.withdraw()  # Hide it until we're ready to draw
+    info_window.update_idletasks()
+    title = " -> ".join([x for x in search_list if not x == "*"])
+    info_window.title('"{}" Info'.format(title))  # set title to search path
+    # Create and place the scrollbars
+    vsb = AutoHideScrollbar(info_window)
+    vsb.grid(row=0, column=1, sticky="ns")
+    # Create a horizontal scroll bar even though we'll never use it, I guess
+    hsb = AutoHideScrollbar(info_window, orient=tk.HORIZONTAL)
+    hsb.grid(row=1, column=0, sticky="ew")
+    # Create, place, and color the text widget
+    text = FormattedText(
+        info_window,
+        yscrollcommand=vsb.set,
+        xscrollcommand=hsb.set,
+        highlightthickness=0,
+        borderwidth=5,
+        wrap=tk.WORD,
+        font=font
+    )
+    text.grid(row=0, column=0, sticky="nsew")
+    text.configure(bg=bg, fg=fg)
+    # Set up the scroll commands
+    vsb.config(command=text.yview)
+    hsb.config(command=text.xview)
+    # Allow the text widget to expand
+    info_window.rowconfigure(0, weight=1)
+    info_window.columnconfigure(0, weight=1)
+
+    # Madness to get the titlebar height
+    offset_y = 0
+    if os.name == "nt":
+        import ctypes
+        try:  # >= win 8.1
+            ctypes.windll.shcore.SetProcessDpiAwareness(2)
+        except:  # win 8.0 or less
+            ctypes.windll.user32.SetProcessDPIAware()
+    offset_y = int(info_window.geometry().rsplit('+', 1)[-1])
+    bar_height = info_window.winfo_rooty() - offset_y
 
     # very basic error checking and display
     # length of zero means no result was found
     # length of one is an error message
-    if len(result) == 0:
-        text.insert("end", "no info found for: ", "bold")
-        text.insert("end", search_list, "bold")
-        return
-#    if len(result) == 1:
-#        text.insert("end", result[0], "reverse")
-#        return
+    if result:
+        style = "normal"
 
-    style = "normal"
+        in_escape = False
+        esc_code = ""
 
-    in_escape = False
-    esc_code = ""
-    for line in result:
         # uncomment print line to get output in console while debugging
         #        print(line.rstrip())
         out = ""  # build output string between esc seq one char at a time
-        for c in line:
+        max_w = line_w = 0  # keep track of the longest line we have
+        total_h = line_h = 0
+
+        def dump_out(out, style):
+            text.insert("end", out, style)
+            line_w = getattr(text, style+"_font").measure(out)
+            line_h = getattr(text, style+"_font").metrics("linespace")
+            return (line_w, line_h)
+
+        for c in result:
             # quick hack to decode the escape seqs ret from the parse
             # only including encodings needed for Configuration.tex and a
             # few others for now
@@ -100,13 +156,57 @@ def display_info_window(config_tex, search_list, width, valid_only, show_urls, m
                 # found end of one esc and start of another
                 # dump formatted output to window
                 # and start over
-                text.insert("end", out, style)
+                w, h = dump_out(out, style)
+                line_w += w
+                line_h = max(line_h, h)
                 out = ""
                 in_escape = True
                 continue
+            # Check for a newline and dump the output
+            if c == "\n":
+                w, h = dump_out(out, style)
+                line_w += w
+                line_h = max(line_h, h)
+                max_w = max(line_w, max_w)
+                total_h += line_h
+                line_w = 0  # reset the line width counter
+                out = ""
             out += c
-        # reached end of line, dump out to window
-        text.insert("end", out, style)
+        if out:
+            w, h = dump_out(out, style)
+            line_w += w
+            line_h = max(line_h, h)
+            max_w = max(line_w, max_w)
+            total_h += line_h
+    else:
+        # Don't show the window - instead, show an error
+        # that nothing was found
+        info_window.destroy()
+        mb.showerror(
+            "Info Not Found",
+            "No info found for: \"{}\"".format(title))
+        return None
+
+    # Fit the line width to our max line length topping out at our width value
+    # text.configure(width=min(max([len(x)+1 for x in text.get("1.0","end-1c").split("\n")]),width))
+    # move window to mx, my
+    # Adjust the width to a fancy mess
+    # max_w += 30
+    max_mono_w = text.mono_font.measure("-"*width)
+    info_window.geometry("{}x{}+{}+{}".format(
+        min((max_w, max_mono_w))+pad_pixels,
+        total_h+bar_height+5,
+        mx,
+        my
+    ))
+    info_window.deiconify()
+    # text.configure(width=final_w)
+    # Adjust the height to the line count
+    # text.configure(height=int(text.index("end-1c").split(".")[0])+1) # Add an extra line for looks
+    # Set the text to read-only
+    text.configure(state="disabled")
+    # Ensure we return a reference to the window for updating colors and stack order checks
+    return info_window
 
 
 def parse_configuration_tex(config_file, search_list, width, valid_only, show_urls):
@@ -248,8 +348,14 @@ def parse_configuration_tex(config_file, search_list, width, valid_only, show_ur
         else:
             if len(parsed_line) != 0:
                 result.append(parsed_line)
+    # Join the result into a single string and remove
+    # leading, trailing, and excessive newlines
+    # result = re.sub(r"\n{2,}",r"\n\n","".join(result))
+    # return result.strip("\n")
 
-    return result
+    # return "".join(result)
+
+    return re.sub("\n{2,}", "\n\n", "".join(result)).strip("\n")
 
 
 def parse_line(line, columns, width, align, valid_only, show_urls):
@@ -290,7 +396,7 @@ def parse_line(line, columns, width, align, valid_only, show_urls):
                 build_key = False
                 if key == "item":
                     if not valid_only:
-                        ret += "•"
+                        ret += u"\u2022"
                 ret += special_char(key)
                 col_contents_len += 1
                 if c in ",()0123456789$":
@@ -326,8 +432,7 @@ def parse_line(line, columns, width, align, valid_only, show_urls):
                 if columns > 0:
                     pad = col_width - col_contents_len - 1
                     if pad > 0:
-                        for _ in range(pad):
-                            ret += " "
+                        ret += " "*pad
                     col_contents_len = 0
                     ret += "|"
                 else:
@@ -342,41 +447,39 @@ def parse_line(line, columns, width, align, valid_only, show_urls):
         ret += special_char(key)
 
     if not valid_only:
-        if key == "tightlint":
+        if key == "tightlist":
             ret = ""
         else:
             if key == "hline":
-                ret = ""
-                for _ in range(width - 4):
-                    ret += "-"
+                ret = "-"*(width-4)
             ret += "\n"
     return ret
 
 
 def special_char(key):
     if key == "kappa":
-        return "\u03f0"
+        return u"\u03f0"
     elif key == "lambda":
-        return "\u03bb"
-    elif key == "mu":
-        return "\u03bc"
+        return u"\u03bb"
+    elif key == "m":
+        return u"\u03bc"
     elif key == "alpha":
-        return "\u03b1"
+        return u"\u03b1"
     elif key == "beta":
-        return "\u03b2"
+        return u"\u03b2"
     elif key == "gamma":
-        return "\u03b3"
+        return u"\u03b3"
     elif key == "leq":
-        return "\u2264"
+        return u"\u2264"
     elif key == "cdot":
-        return "\u00b7"
+        return u"\u00b7"
     elif key == "in":
-        return "\u220a"
+        return u"\u220a"
     elif key == "infty":
-        return "\u221e"
+        return u"\u221e"
     elif key == "textbackslash":
         return "\\"
     elif key == "hline":
-        return "\u200b"
+        return u"\u200b"
     else:
         return " "
