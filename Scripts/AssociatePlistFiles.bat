5a44ceb6d1a2f30ba0a355cb9324377d7e4ccd1b
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2024-05-29 15:28:30 -0500

    Migrate from VBScript to Powershell

--- Scripts/AssociatePlistFiles.bat
+++ Scripts/AssociatePlistFiles.bat
@@ -1,14 +1,21 @@
-<!-- : Begin batch script
-
 @echo off
+
+set "this_script=%~f0"
+set "this_dir=%~dp0"
+set "args=%*"
+set "passed_script=%~1"
+set "_script_no_ext=%~nx1"
+set "_script_name=%~n1"
+
 setlocal enableDelayedExpansion
 
-:checkPrivileges
-net file 1>nul 2>nul
-if "%errorlevel%" == "0" ( goto gotPrivileges ) else ( goto getPrivileges )
+goto getPrivileges
 
 :getPrivileges
-cscript //nologo "%~f0?.wsf" //job:ADMIN "--self" %*
+call :launchScript "!this_script!" "args"
+if "%errorlevel%" == "0" (
+    goto gotPrivileges
+)
 exit /b
 
 :gotPrivileges
@@ -16,16 +23,16 @@ cls
 REM See if we have a custom script passed
 set "target=ProperTree.bat"
 set "name=ProperTree"
-if NOT "%~1" == "" (
-    set "target=%~nx1"
-    set "name=%~n1"
+if NOT "!passed_script!" == "" (
+    set "target=!_script_no_ext!"
+    set "name=!_script_name!"
 )
 REM Get one directory up
-pushd %~dp0
+pushd "!this_dir!"
 cd ..\
 REM Ensure the target exists
-if NOT EXIST "%target%" (
-    echo Could not find %target%.
+if NOT EXIST "!target!" (
+    echo Could not find !target!.
     echo Please make sure to run this script from ProperTree's Scripts Folder.
     echo.
     echo Press [enter] to quit.
@@ -35,59 +42,67 @@ if NOT EXIST "%target%" (
 set "path=%cd%"
 set "regpath=%ComSpec:cmd.exe=%reg.exe"
 popd
-echo Checking if %target% exists in registry...
-"!regpath!" query "HKCR\Applications\%target%" > nul 2>&1
+echo Checking if !target! exists in registry...
+"!regpath!" query "HKCR\Applications\!target!" > nul 2>&1
 if "%errorlevel%"=="0" (
     echo  - Already exists.  Removing...
     echo.
-    "!regpath!" delete "HKCR\Applications\%target%" /f 2> nul
+    "!regpath!" delete "HKCR\Applications\!target!" /f 2> nul
     "!regpath!" delete "HKCR\.plist_auto_file" /f 2> nul
     "!regpath!" delete "HKCR\.plist" /f 2> nul
-    "!regpath!" delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\%target%_.plist" /f 2> nul
+    "!regpath!" delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\!target!_.plist" /f 2> nul
 )
-set arg=\"%path%\%target%\" \"%%1\"
+set arg=\"%path%\!target!\" \"%%1\"
 set "icon=%path%\Scripts\shortcut.ico"
 echo.
 echo Adding registry values...
 echo.
-"!regpath!" add "HKCR\Applications\%target%\shell\Open" /t REG_SZ /d "Open with %name%" /f
-"!regpath!" add "HKCR\Applications\%target%\shell\Open\command" /t REG_SZ /d "%arg%" /f
+"!regpath!" add "HKCR\Applications\!target!\shell\Open" /t REG_SZ /d "Open with !name!" /f
+"!regpath!" add "HKCR\Applications\!target!\shell\Open\command" /t REG_SZ /d "%arg%" /f
 "!regpath!" add "HKCR\.plist" /t REG_SZ /d ".plist_auto_file" /f
-"!regpath!" add "HKCR\.plist_auto_file\shell\Open" /t REG_SZ /d "Open with %name%" /f
+"!regpath!" add "HKCR\.plist_auto_file\shell\Open" /t REG_SZ /d "Open with !name!" /f
 "!regpath!" add "HKCR\.plist_auto_file\shell\Open\command" /t REG_SZ /d "%arg%" /f
 "!regpath!" add "HKCR\.plist_auto_file\DefaultIcon" /t REG_SZ /d "%icon%" /f
-"!regpath!" add "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\%target%_.plist" /t REG_DWORD /d 0 /f
+"!regpath!" add "HKCU\Software\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts" /v "Applications\!target!_.plist" /t REG_DWORD /d 0 /f
 echo.
 echo Press [enter] to exit...
 pause > nul
 exit /b
 
------ Begin wsf script --->
+:launchScript <script_path> <args_name>
+setlocal enableDelayedExpansion
+REM Check if we have admin
+net file 1>nul 2>nul
+if "%errorlevel%" == "0" (
+    exit /b 0
+)
+REM Let's check what our requirements are in order to launch, and
+REM adjust as needed
+set "_target=%~1"
+set "_args=!%~2!"
+REM Let's sanitize our args a bit
+if not "!_args!" == "" (
+    call :cleanArg "_args" "true"
+)
+REM And our target
+call :cleanArg "_target"
+REM Let's build our command
+powershell -c "& {$a=@();foreach($x in $args){foreach($y in (Invoke-Expression('Write-Output -- '+$x-replace'\$',\"`0\"))-replace\"`0\",'$$'){$a+='\"{0}\"'-f$y}};$c=$a-join' ';$d='/c \"{0}\"'-f$c;Start-Process '%COMSPEC:'=''%' -Verb RunAs -ArgumentList $d}" '!_target!!_args!'
+endlocal
+exit /b 1
 
-<package>
-    <job id="ADMIN">
-        <script language="VBScript">
-            dim self_path: self_path = WScript.ScriptFullName
-            If StrComp(Right(self_path,5),"?.wsf",vbTextCompare)=0 Then: self_path = Left(self_path,Len(self_path)-5): End If
-            Set argument_list = CreateObject("System.Collections.ArrayList")
-            For Each argument_item in WScript.Arguments
-                If StrComp(argument_item,"--self",vbTextCompare)=0 Then: argument_item = self_path: End If
-                Call argument_list.Add(argument_item)
-            Next
-            If argument_list.Count=0 Then: Call argument_list.Add(self_path): End If
-            dim target_exe: target_exe = "cmd.exe"
-            If StrComp(Right(argument_list(0),4),".exe",vbTextCompare)=0 Then
-                target_exe = argument_list(0)
-                Call argument_list.RemoveAt(0)
-            ElseIf StrComp(Right(argument_list(0),4),".bat",vbTextCompare)=0 or StrComp(Right(argument_list(0),4),".cmd",vbTextCompare)=0 Then
-                Call argument_list.Insert(0,"/c")
-            End If
-            dim args: args = ""
-            For Each a in argument_list
-                if args<>"" Then: args = args & " ": End If
-                args = args & chr(34) & a & chr(34)
-            Next
-            CreateObject("Shell.Application").ShellExecute target_exe, args, , "runas", 5
-        </script>
-    </job>
-</package>
+:cleanArg <argname> <prepend_space>
+REM Escape some problematic chars for powershell
+set "%~1=!%~1:`=``!"
+set "%~1=!%~1:'=`''!"
+set "%~1=!%~1:"=\"!"
+set "%~1=!%~1:(=`(!"
+set "%~1=!%~1:)=`)!"
+set "%~1=!%~1:{=`{!"
+set "%~1=!%~1:}=`}!"
+if "%~2" == "" (
+    set "%~1=!%~1!"
+) else (
+    set "%~1= !%~1!"
+)
+exit /b
